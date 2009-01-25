9c977a1447190393ae16d599a1c4fd7c9ce1ba09
Yashwanth Nelapati <yash888@gmail.com>
2009-01-25 23:00:41 +0000

    Adding setup.py. Thanks for the patch Jannis.
    git-svn-id: http://django-locations.googlecode.com/svn/trunk@17 7dd5b791-a154-0410-b265-8553752079d8

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-locations',
+    version='0.1.0',
+    description='A location based social network using django & pinax',
+    author='Yashh',
+    author_email='yash888@gmail.com',
+    url='http://code.google.com/p/django-locations/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
