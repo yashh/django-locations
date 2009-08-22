6bb1df9694a178b75f48e6dde2d1e797efbaf195
Brian Rosner <brosner@gmail.com>
2009-08-22 10:27:06 -0600

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-locations',
-    version='0.1.0',
+    version='0.1.1',
     description='A location based social network using Django & Pinax',
     author='Yashwanth Nelapati',
     author_email='yash888@gmail.com',
